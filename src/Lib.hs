6fa8e53429b2ac1665c671b66e3f8beb408fe3ec
okoshovetc <okoshovetc@gmail.com>
2017-04-04 23:07:47 +0300

    seems like a good way to print a map

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,32 +15,33 @@ data GameBox = GameBox {
 printCell     :: Cell -> String
 endLineFilter :: Cell -> Bool -> String
 printMap      :: GameBox -> String
-_printMap     :: GameMap -> MapSize -> Int -> String
+insertElem    :: Int -> a -> [a] -> [a]
+
+insertElem _ _  [] = []
+insertElem i e x = (take i x) ++ [e] ++ insertElem i e (drop i x)
 
 run :: IO ()
-run = putStrLn (printMap generateMap)
-
-printMap gb = _printMap (gameMap gb) (width gb) 0
-
-_printMap [] _ _ = ""
-_printMap (x:xs) w i = ( endLineFilter x True ) + (_printMap xs w i + 1)
+run = putStrLn (printMap (generateMap))
 
 
-endLineFilter cell True  = (printCell cell) + "\n"
+printMap gb = foldl (++) "" (insertElem (width gb) "\n" (map printCell (gameMap gb)))
+
+
+endLineFilter cell True  = (printCell cell)
 endLineFilter cell False = (printCell cell)
 
 
 printCell EMPTY   = " "
-printCell WALL    = "▩"
-printCell BOX     = "□"
-printCell GOAL    = "◯"
+printCell WALL    = "#"
+printCell BOX     = "@"
+printCell GOAL    = "X"
 printCell PERSON  = "&"
 
 
 generateMap :: GameBox
 generateMap = GameBox
     {
-        gameMap = [ EMPTY ],
-        width   = 1,
+        gameMap = [WALL, WALL, WALL, WALL, WALL, PERSON, BOX, WALL, WALL, WALL, WALL, WALL],
+        width   = 4,
         height  = 1
     }

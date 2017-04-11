3c0250d7daa8bd77078ea181d85c4e2cf16d5b5a
Vladislav Grubov <vladgrubov@gmail.com>
2017-04-11 13:10:08 +0300

    Graphics.Gloss imported to project. Some warnings while comilation is possible

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,13 @@ module Lib where
 import Printer(printMap)
 import Types
 
+import Graphics.Gloss.Geometry.Line
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Interface.Pure.Simulate
+
+
 run :: IO ()
 run = putStrLn (printMap (generateMap))
-
 
 generateMap :: GameBox
 generateMap = GameBox
@@ -14,3 +18,20 @@ generateMap = GameBox
         width   = 4,
         height  = 1
     }
+
+-- leftDown  :: GameBox -> GameBox
+-- leftDown  gb = gb
+-- rightDown :: GameBox -> GameBox
+-- rightDown gb = gb
+-- leftUp    :: GameBox -> GameBox
+-- leftUp    gb = gb
+-- rightUp   :: GameBox -> GameBox
+-- rightUp   gb = gb
+
+
+-- handle :: Event -> GameBox -> GameBox
+-- handle (EventKey (SpecialKey KeyLeft) Down _ _) u   = leftDown u
+-- handle (EventKey (SpecialKey KeyRight) Down _ _) u  = rightDown u
+-- handle (EventKey (SpecialKey KeyLeft) Up _ _) u     = leftUp u
+-- handle (EventKey (SpecialKey KeyRight) Up _ _) u    = rightUp u
+-- handle _ u = u
